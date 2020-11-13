fbb4e96ec77caf1a44739fce7dbeb73caaaa5597
AWSiegfried <awsiegfried@gmail.com>
2020-11-13 13:18:29 -0800

    updated artist array

--- awsiegfried.js
+++ awsiegfried.js
@@ -1,17 +1,35 @@
 $(document).ready(function() {
 
     var artistArray = [{
-            "name": "Bodily Ruin"
+            name: "Bodily Ruin",
+            genre: "metal",
+            bio: "'Death, at its finest, blossoms in a place that's cold and dark, where a candle offers grace.'  Bodily Ruin is a brutal death metal band from North Hollywood, that putts on a spectacle every night they play with lit candles to set the ambiance and ear-piercing death whistles to give you goosebumps while you mosh.",
         },
         {
-            "name": "Syn Abscence"
+            name: "Vexxum",
+            genre: "metal",
+            bio: "Death/Thrash metal band from West Conva, CA.  With heavy chugged riffs and duo guitar harmonies and guitar solos backed by crips, heavy and hard hitting bass and insane drums, Vexxum is bound to bang your head and start the pit.  Vexxum placed first in the opening round of the 2020 Wacken Metal Battle USA Battle of the Bands.",
         },
         {
-            "name": "Holy Wretched"
+            name: "Triangle Fire",
+            genre: "rock",
+            bio: "Triangle Fire is an Alternative Rock and Roll band based in East Los Angeles, comprised of singer-songwriter Robert Abalos and multi-instrumentalists Ian Rosales, Salvador Trejo Jr., and Emilio Moyao.  Their debut record 'Everything Works' is a manifestation of human perseverance through trials of loneliness, failure, and mental illness, holding an optimism that hope can survive beyond the valley of death.  It's fun stuff yo."
         },
         {
-            "name": "Vexxum"
+            name: "The Absurd",
+            genre: "rock",
+            bio: "The Absurd is a true power trio, firing off catchy hooks and thunderous riffs with equal potency.  Originating from Detroit, now based in Los Angeles, the band is known for their explosive live show.  They've 'melted the paint off the walls at just about every club' they've played, and have been featured on stages ranging from Music Festivals to Gay Pride to underground art galleries.  Their debut LP was released in 2018, meriting inclusion in multiple end-of-year lists.  Their second album, 'The Sun Still Rises' dropped November 2nd, 2020.  'A conflagration of heavy psychedelia that sees Foerg caterwauling over the daunting muscle-flexing of his rhythm section.'"
         },
+        {
+            name: "Stackztootrill",
+            genre: "rap",
+            bio: "3600 boss Stackztootrill is a Los Angeles, CA based artist who has been making waves & setting trends in hip hop from his very first song (Juice).  One of the most versatile artists to hail from California you never know what exactly to expect from a Stackztootrill track which keeps his fans coming back time and again.  Whether it's a banging track like Wave or a crooner love story such as Slidin, All throughout giving his pains and turning it into light, (The Spark) on tracks."
+        },
+        {
+            name: "QU/NTEN",
+            genre: "rap",
+            bio: "You may remember QU/NTEN from his bumping song “Route 66.” The young, then 18-year-old, artist impressed us with his mature sound that was melodic and full of summertime drive vibes. What was even more impressive was learning that not only was he creating a legit full body of work. But, that he was doing it all on his own being his producer, writer, and mixing and mastering his songs.  Now in 2020 QU/NTEN is stepping out with his new EP “Forever Slept On.” A 7 track 14-minute project gives a wider lens of which to view his music. Giving us some new new while still coming with that melodic and visceral experience that captivated us before."
+        }
     ];
 
 
